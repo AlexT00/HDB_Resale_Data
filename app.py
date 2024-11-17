1ce8820b8b51e42438a8b2638e49b5ed2bb868f6
Alex Tok <alexander.tokjs@gmail.com>
2024-11-17 15:27:09 +0800

    Delete unrelevant files

--- app.py
+++
@@ -1,6 +0,0 @@
-import sys
-import HDB_PC_Parser
-if __name__ == "__main__":
-    parser = HDB_PC_Parser.HDB_PC_Parser("HDBPropertyInformation.csv")
-    data = parser.apply_df(parser.data_file_name)
-    data.to_csv("HDB_Postal_Code_Edit.csv", index=False)
